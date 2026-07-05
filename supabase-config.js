ab0ee7f03552a015eed60312535cc5ebcf2bdd5e
PJKaraffa <61504379+PJKaraffa@users.noreply.github.com>
2026-07-05 19:58:57 -0400

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://uzysizpltsqpbngwywxs.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_VJHnzaYYkCHomnfJ70ffDw_zgUHfxwE";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
