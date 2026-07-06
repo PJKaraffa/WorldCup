c96052497c12abe33e8ee57759e624bd2b8febf6
PJKaraffa <61504379+PJKaraffa@users.noreply.github.com>
2026-07-05 20:18:37 -0400

    Add files via upload

---
+++ login.js
@@ -0,0 +1,29 @@
+async function signUp() {
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  const { error } = await supabaseClient.auth.signUp({
+    email,
+    password
+  });
+
+  document.getElementById("loginMessage").innerText =
+    error ? error.message : "Account created. Check your email.";
+}
+
+async function login() {
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  const { error } = await supabaseClient.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    document.getElementById("loginMessage").innerText = error.message;
+    return;
+  }
+
+  window.location.href = "index.html";
+}
